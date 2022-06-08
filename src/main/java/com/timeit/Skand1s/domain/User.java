a761998770a4beebd79b809db2f83cf8bb1a43a2
unknown <djabc.pwc@gmail.com>
2022-06-08 10:15:25 +0200

    changed username to uniqut

--- src/main/java/com/timeit/Skand1s/domain/User.java
+++ src/main/java/com/timeit/Skand1s/domain/User.java
@@ -16,6 +16,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long user_id;
 
+    @Column(unique = true)
     private String username;
     private String password;
     private boolean enabled;
